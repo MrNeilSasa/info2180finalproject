4faa1006629de6f9f1b056fcd3e982d43f4371e4
Antonio Neil <bossneil20@gmail.com>
2022-12-03 16:48:39 -0500

    Added logout file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
+
     $("#loginbtn").on('click', function(event){
         event.preventdefault();
+
+        var email = $('#email');
+        var password = $('password');
     })
 });
