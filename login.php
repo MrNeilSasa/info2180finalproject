f61c806dc66da1ae003e9607e9f68db15da4cbe2
Antonio Neil <bossneil20@gmail.com>
2022-12-02 22:09:07 -0500

    Updated starter code part 2

--- login.php
+++ login.php
@@ -11,7 +11,25 @@ $password = $_POST['password'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 </head>
+<header>
+    <h1> Dolphin CRM </h1>
+</header>    
 <body>
+    <div class="dashboard-container">
+        <h2>Dashboard </h2>
+        <button type="button" id="addBtn" class="addbtn">Add Contact </button> 
+    </div>    
     
+    <aside class="nav-bar">
+        <ul>
+            <li><a href="login.php">Home</a></li>
+            <li><a href="contact.php">New Contact</a></li>
+            <li><a href="users.php">Example.com</a></li>
+
+        </ul>
+        <a href="index.html"> Logout </a>
+    </aside>
+
+
 </body>
 </html>
