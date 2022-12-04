55cc4b25ee140872e383a5bcd185e757609290ab
Romario2Crosbie <romario.anonymous2000@gmail.com>
2022-12-04 17:41:19 -0500

    test

--- dashboard.php
+++ dashboard.php
@@ -1,40 +1,5 @@
 <?php
 
-$con = new mysqli('localhost', 'root', '','dolphin_crm');
-
-/*$host = 'localhost';
-$username = 'info2180proj';
-$password = 'password123';
-$dbname = 'dolphin_crm';
-error_reporting(0);
-$all = $_GET['all'];
-
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);*/
-
-?>
-
-<?php
-    /*$stmt = $conn -> query("SELECT * contacts");
-    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-    ?>
-
-    <table class='table'>
-  <thread>
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Company</th>
-      <th>Type</th>
-    </tr>
-  </thread>
-  <tbody>
-<?php foreach($result as $row): ?>
-      <tr>
-        <td><?=$row['title']; ?></td>
-        <td><?=$row['email']; ?></td>
-        <td><?=$row['company']; ?></td>
-        <td><?=$row['type']; ?></td>
-
-      </tr>
-    <?php endforeach; ?>
-  </tbody>*/
+$con = new mysqli('localhost', 'root', '','dolphin_crm'); 
+if(!$con){
+    die (mysqli_error($con));
