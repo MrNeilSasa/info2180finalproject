57a9e1d2cf43e3da2ce782c9e66c2433c68f3f95
Antonia <53801502+coderabc1126@users.noreply.github.com>
2022-12-06 19:56:02 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -45,7 +45,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" a href="contact.css">
-    <script src="app.js" crossorigin="anonymous"></script>
+    <script src="contact.js" crossorigin="anonymous"></script>
     <title>New Contact</title>
 </head>
 <header>
